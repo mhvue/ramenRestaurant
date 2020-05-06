136da2c1fe6767d4fd3a9b168a2c30e0022fad69
Mai Houa Vue <mhvue4@gmail.com>
2020-05-05 22:31:54 -0500

    starting funct. to add grab value from onclick on button

--- js/app.js
+++ js/app.js
@@ -1,5 +1,13 @@
+//contact modal 
 $("#contactBtn").on("click", function(){
     $("#contactModal").modal("toggle");
 
 });
+
+//make it so each button's value is grabbed individually to orderHolder
+$("#orderOne").on("click", function(){
+    console.log($("#btnData").parent().find(".name").text());
+});
+
+
 
