71a8ccc03f19c98e54aa03d6de18204e347eceb7
Mai Houa Vue <mhvue4@gmail.com>
2020-05-26 22:27:52 -0500

    dynamically created Order buttons instead in js

--- js/app.js
+++ js/app.js
@@ -1,13 +1,32 @@
+
 //contact modal 
 $("#contactBtn").on("click", function(){
     $("#contactModal").modal("toggle");
 
 });
 
-//make it so each button's value is grabbed individually to orderHolder
-$("#orderOne").on("click", function(){
-    console.log($("#btnData").parent().find(".name").text());
-});
 
+count = 0
+for(var i = 0; i < 6; i++){
+    count ++
+    var buttonData= $("<button>").text("Order").addClass("orderBtn").attr("id", "ramen" + count)
+    
+
+}
+$(".btnDataHolder").append(buttonData)
+
+
+
+// $(".priceData").append(buttonData)
+// var name = $("#btnData" + count).parent().find(".name").text();
+// var price = $("#btnData" + count).parent().find("#price").text();
+
+//dynamically adding buttons 
+
+//make it so each button's value is grabbed individually to orderHolder
+$(document.body).on("click",".orderBtn", function(){
+    
+    // $(".orderHolder").append(name + price);
+});
 
 
