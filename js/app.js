74a68e3631665ce26aaa974d5f6de1581b51b4cf
Mai Houa Vue <mhvue4@gmail.com>
2020-06-07 19:56:52 -0500

    updated to add ramen menu items dynamtically instead via js

--- js/app.js
+++ js/app.js
@@ -1,32 +1,54 @@
-
 //contact modal 
-$("#contactBtn").on("click", function(){
+$("#contactBtn").on("click", function () {
     $("#contactModal").modal("toggle");
 
 });
 
 
-count = 0
-for(var i = 0; i < 6; i++){
-    count ++
-    var buttonData= $("<button>").text("Order").addClass("orderBtn").attr("id", "ramen" + count)
-    
+const ramenMenu = {
+    ramenOne: ["1", "Miso Ramen", "pic", "afjakakjal", 12.99],
+    ramenTwo: ["2", "Spicy Ramen", "pic", "afjakakjal", 13.99],
+    ramenThree: ["3", "House Ramen", "pic", "afjakakjal", 14.99],
+    ramenFour: ["4", "Supreme Ramen", "pic", "afjakakjal", 15.99],
+    ramenFive: ["5", "Normal Ramen", "pic", "afjakakjal", 12.99],
+}
+
+for (let i = 0; i < 10; i++) {
+
+    const menuRow =
+        $("#dataRow1").append(
+            $("<td>").text(ramenMenu.ramenOne[i])
+        )
+    const menuRowTwo =
+        $("#dataRow2").append(
+            $("<td>").text(ramenMenu.ramenTwo[i])
+        )
+    const menuRowThree =
+        $("#dataRow3").append(
+            $("<td>").text(ramenMenu.ramenThree[i])
+        )
+    const menuRowFour=
+        $("#dataRow4").append(
+            $("<td>").text(ramenMenu.ramenFour[i])
+        )
+    const menuRowFive=
+    $("#dataRow5").append(
+        $("<td>").text(ramenMenu.ramenFive[i])
+    )
+
+
+    $(".menuTable").append(menuRow, menuRowTwo, menuRowThree, menuRowFour, menuRowFive);
+
 
 }
-$(".btnDataHolder").append(buttonData)
 
 
 
-// $(".priceData").append(buttonData)
-// var name = $("#btnData" + count).parent().find(".name").text();
-// var price = $("#btnData" + count).parent().find("#price").text();
 
-//dynamically adding buttons 
 
 //make it so each button's value is grabbed individually to orderHolder
-$(document.body).on("click",".orderBtn", function(){
-    
+$(document.body).on("click", ".orderBtn", function () {
+    console.log("click")
+
     // $(".orderHolder").append(name + price);
 });
-
-
