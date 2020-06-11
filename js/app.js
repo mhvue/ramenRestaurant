95dfc84097eab723f25b33075eff835b05c365de
Mai Houa Vue <mhvue4@gmail.com>
2020-06-10 20:54:59 -0500

    got menu price items to show in the modal

--- js/app.js
+++ js/app.js
@@ -6,11 +6,11 @@ $("#contactBtn").on("click", function () {
 
 
 const ramenMenu = {
-    ramenOne: ["1", "Miso Ramen", "pic", "afjakakjal", 12.99,"<button id= '1'>Order</button>"],
-    ramenTwo: ["2", "Spicy Ramen", "pic", "afjakakjal", 13.99, "<button id= '2'>Order</button>"],
-    ramenThree: ["3", "House Ramen", "pic", "afjakakjal", 14.99, "<button id= '3'>Order</button>"],
-    ramenFour: ["4", "Supreme Ramen", "pic", "afjakakjal", 15.99, "<button id= '4'>Order</button>"],
-    ramenFive: ["5", "Normal Ramen", "pic", "afjakakjal", 12.99, "<button id= '5'>Order</button>"]
+    ramenOne: ["1", "Miso Ramen", "pic", "afjakakjal","<p id='price1'> 12.99 </p>", "<button id= '1'>Order</button>"],
+    ramenTwo: ["2", "Spicy Ramen", "pic", "afjakakjal", "<p id='price2'> 13.99 </p>", "<button id= '2'>Order</button>"],
+    ramenThree: ["3", "House Ramen", "pic", "afjakakjal", "<p id='price3'> 14.99 </p>", "<button id= '3'>Order</button>"],
+    ramenFour: ["4", "Supreme Ramen", "pic", "afjakakjal","<p id='price4'> 12.99 </p>", "<button id= '4'>Order</button>"],
+    ramenFive: ["5", "Normal Ramen", "pic", "afjakakjal", "<p id='price5'> 11.99 </p>", "<button id= '5'>Order</button>"]
 }
 
 for (let i = 0; i < 6; i++) {
@@ -44,9 +44,11 @@ for (let i = 0; i < 6; i++) {
 //make it so each button's value is grabbed individually to orderHolder
 $("button").on("click", function () {
     const getInfo = $(this).attr("id");
-    $(".modal-body").append("<br>").append( getInfo);
+    const getPrice= $(this).parent().parent().find("p").text()
+    console.log(getPrice)
+    $(".modal-body").append("<br>").append(getInfo, "-", getPrice);
     $("#orderModal").modal("toggle");
-    console.log(getInfo)
+    
   
 });
 
