2a42a934ab0dc370d510ab8a50a2575abc64fb54
Mai Houa Vue <mhvue4@gmail.com>
2020-06-11 21:29:02 -0500

    added images of ramen items

--- js/app.js
+++ js/app.js
@@ -6,18 +6,18 @@ $("#contactBtn").on("click", function () {
 
 
 const ramenMenu = {
-    ramenOne: ["1", "Miso Ramen", "pic", "afjakakjal","<p id='price1'> 12.99 </p>", "<button class='orderBtn' id= '1'>Order</button>"],
-    ramenTwo: ["2", "Spicy Ramen", "pic", "afjakakjal", "<p id='price2'> 13.99 </p>", "<button class='orderBtn'  id= '2'>Order</button>"],
-    ramenThree: ["3", "House Ramen", "pic", "afjakakjal", "<p id='price3'> 14.99 </p>", "<button class='orderBtn'  id= '3'>Order</button>"],
-    ramenFour: ["4", "Supreme Ramen", "pic", "afjakakjal","<p id='price4'> 12.99 </p>", "<button class='orderBtn' id= '4'>Order</button>"],
-    ramenFive: ["5", "Normal Ramen", "pic", "afjakakjal", "<p id='price5'> 11.99 </p>", "<button class='orderBtn' id= '5'>Order</button>"]
+    ramenOne: ["1", "Miso Ramen", "<img src='./assets/images/ramen1.jpg'>", "Lorem ipsum dolor sit amet, consectetur adipiscing elit.","<p id='price1'> 12.99 </p>", "<button class='orderBtn' id= '1'>Order</button>"],
+    ramenTwo: ["2", "Spicy Ramen", "<img src='./assets/images/ramen2.jpg'>", "Lorem ipsum dolor sit amet, consectetur adipiscing elit.", "<p id='price2'> 13.99 </p>", "<button class='orderBtn'  id= '2'>Order</button>"],
+    ramenThree: ["3", "House Ramen", "<img src='./assets/images/ramen3.jpg'>", "Lorem ipsum dolor sit amet, consectetur adipiscing elit.", "<p id='price3'> 14.99 </p>", "<button class='orderBtn'  id= '3'>Order</button>"],
+    ramenFour: ["4", "Supreme Ramen", "<img src='./assets/images/ramen4.jpg'>", "Lorem ipsum dolor sit amet, consectetur adipiscing elit.","<p id='price4'> 12.99 </p>", "<button class='orderBtn' id= '4'>Order</button>"],
+    ramenFive: ["5", "Normal Ramen", "<img src='./assets/images/ramen5.jpg'>", "Lorem ipsum dolor sit amet, consectetur adipiscing elit.", "<p id='price5'> 11.99 </p>", "<button class='orderBtn' id= '5'>Order</button>"]
 }
 
 for (let i = 0; i < 6; i++) {
 
     const menuRow =
         $("#dataRow1").append(
-            $("<td>").append(ramenMenu.ramenOne[i])
+            $("<td>").html(ramenMenu.ramenOne[i])
         )
     const menuRowTwo =
         $("#dataRow2").append(
@@ -53,7 +53,7 @@ $(".orderBtn").on("click", function () {
         return a + b;
     },0);
 
-    $("#totalHere").html(total)
+    $("#totalHere").html("Total: " + total)
     $("#orderModal").modal("toggle");
     
 });
