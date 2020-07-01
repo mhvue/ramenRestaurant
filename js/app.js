ef732f38ceb9192d4cd72ae195279ae807b50c6b
Mai Houa Vue <mhvue4@gmail.com>
2020-06-30 22:21:18 -0500

    added in arrays for ramen name, img , description and order button for cleaner look

--- js/app.js
+++ js/app.js
@@ -4,16 +4,65 @@ $("#contactBtn").on("click", function () {
 
 });
 
+$("#ourStoryP").hide();
 
-const ramenMenu = {
-    ramenOne: ["1", "Miso Ramen", "<img src='./assets/images/ramen1.jpg'>", "Lorem ipsum dolor sit amet, consectetur adipiscing elit.","<p id='price1'> 12.99 </p>", "<button class='orderBtn' id= '1'>Order</button>"],
-    ramenTwo: ["2", "Spicy Ramen", "<img src='./assets/images/ramen2.jpg'>", "Lorem ipsum dolor sit amet, consectetur adipiscing elit.", "<p id='price2'> 13.99 </p>", "<button class='orderBtn'  id= '2'>Order</button>"],
-    ramenThree: ["3", "House Ramen", "<img src='./assets/images/ramen3.jpg'>", "Lorem ipsum dolor sit amet, consectetur adipiscing elit.", "<p id='price3'> 14.99 </p>", "<button class='orderBtn'  id= '3'>Order</button>"],
-    ramenFour: ["4", "Supreme Ramen", "<img src='./assets/images/ramen4.jpg'>", "Lorem ipsum dolor sit amet, consectetur adipiscing elit.","<p id='price4'> 12.99 </p>", "<button class='orderBtn' id= '4'>Order</button>"],
-    ramenFive: ["5", "Normal Ramen", "<img src='./assets/images/ramen5.jpg'>", "Lorem ipsum dolor sit amet, consectetur adipiscing elit.", "<p id='price5'> 11.99 </p>", "<button class='orderBtn' id= '5'>Order</button>"]
+$("#mainInfo").on("click", function(){
+    $("#ourStoryP").fadeIn("slow");
+    $("#ourStoryHeader").hide();
+});
+
+
+
+const ramenName = {
+    miso: "Miso Ramen",
+    spicy: "Spicy Ramen",
+    house: "House Ramen",
+    supreme: "Supreme Ramen",
+    normal: "Normal Ramen"
 }
 
-for (let i = 0; i < 6; i++) {
+const ramenImg ={
+    miso:"<img src='./assets/images/ramen1.jpg' alt='ramen with greens'>",
+    spicy: "<img src='./assets/images/ramen2.jpg' alt='ramen with egg and naruto'>",
+    house: "<img src='./assets/images/ramen3.jpg' alt= 'ramen with egg and pork'>",
+    supreme: "<img src='./assets/images/ramen4.jpg' alt='ramen with beef'>",
+    normal:  "<img src='./assets/images/ramen5.jpg' alt='ramen with red sauce'>"
+}
+
+const ramenInfo={
+    miso: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    spicy: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    house: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    supreme: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    normal: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+}
+
+const ramenPrice = {
+    miso: "<p id='price1'> 12.99 </p>",
+    spicy: "<p id='price2'> 13.99 </p>",
+    house: "<p id='price3'> 14.99 </p>",
+    supreme: "<p id='price4'> 12.99 </p>",
+    normal: "<p id='price5'> 11.99 </p>"
+}
+
+const orderButton ={
+    miso: "<button class='orderBtn' id= '1'>Order</button>",
+    spicy: "<button class='orderBtn'  id= '2'>Order</button>",
+    house: "<button class='orderBtn'  id= '3'>Order</button>",
+    supreme: "<button class='orderBtn' id= '4'>Order</button>",
+    normal: "<button class='orderBtn' id= '5'>Order</button>"
+}
+
+const ramenMenu = {
+    ramenOne: ["1", ramenName.miso, ramenImg.miso, ramenInfo.miso, ramenPrice.miso, orderButton.miso],
+    ramenTwo: ["2", ramenName.spicy, ramenImg.spicy, ramenInfo.spicy, ramenPrice.spicy, orderButton.spicy],
+    ramenThree: ["3", ramenName.house, ramenImg.house , ramenInfo.house, ramenPrice.house, orderButton.house],
+    ramenFour: ["4", ramenName.supreme, ramenImg.supreme, ramenInfo.supreme, ramenPrice.supreme, orderButton.supreme],
+    ramenFive: ["5", ramenName.normal, ramenImg.normal, ramenInfo.normal, ramenPrice.normal, orderButton.normal]
+}
+
+
+for (let i = 0; i < 7; i++) {
 
     const menuRow =
         $("#dataRow1").append(
